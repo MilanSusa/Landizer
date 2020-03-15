67a3f74edb7254486645bc6f11bf38f1c046d146
Milan <milan_susa@hotmail.com>
2020-03-15 16:12:52 +0100

    Update queries.sql to support operations for prediction.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,8 +1,7 @@
 -- :name create-user! :! :n
 -- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+INSERT INTO users (first_name, last_name, email, password)
+VALUES (:first_name, :last_name, :email, :password)
 
 -- :name update-user! :! :n
 -- :doc updates an existing user record
@@ -12,10 +11,45 @@ WHERE id = :id
 
 -- :name get-user :? :1
 -- :doc retrieves a user record given the id
-SELECT * FROM users
+SELECT *
+FROM users
 WHERE id = :id
+
+-- :name get-user-by-email :? :1
+-- :doc retrieves a user record given the email
+SELECT *
+FROM users
+WHERE email = :email
 
 -- :name delete-user! :! :n
 -- :doc deletes a user record given the id
 DELETE FROM users
+WHERE id = :id
+
+-- :name create-prediction! :! :n
+-- :doc creates a new prediction record
+INSERT INTO predictions (landmark, probability, image, user_id)
+VALUES (:landmark, :probability, :image, :user_id)
+
+-- :name update-prediction! :! :n
+-- :doc updates an existing prediction record
+UPDATE predictions
+SET landmark = :landmark, probability = :probability, image = :image user_id = :user_id
+WHERE id = :id
+
+-- :name get-prediction :? :1
+-- :doc retrieves a prediction record given the id
+SELECT *
+FROM predictions
+WHERE id = :id
+
+-- :name get-predictions-for-user :? :n
+-- :doc retrieves prediction records given the user id
+SELECT *
+FROM predictions
+WHERE user_id = :user_id
+
+-- :name delete-prediction! :! :n
+-- :doc deletes a prediction record given the id
+DELETE FROM predictions
 WHERE id = :id
