af7bc5c64662724f83784ee73edac0f78c3f06da
Milan <milan_susa@hotmail.com>
2020-03-19 21:52:28 +0100

    Change data type of image field in predictions table.

--- resources/migrations/20200314230023-add-predictions-table.up.sql
+++ resources/migrations/20200314230023-add-predictions-table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE predictions (
  id SERIAL PRIMARY KEY,
  landmark VARCHAR(50),
  probability NUMERIC,
- image BYTEA,
+ image VARCHAR(255),
  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
