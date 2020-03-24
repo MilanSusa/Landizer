d40cf708dd3338a235ed722b37146d463718fe7f
Milan <milan_susa@hotmail.com>
2020-03-24 16:22:29 +0100

    Add tests for HugSQL operations on predictions table.

--- test/clj/landizer/test/db/core.clj
+++ test/clj/landizer/test/db/core.clj
@@ -83,3 +83,57 @@
                                                  t-conn
                                                  {:id user-id})
                                                :password)))))))
+
+(deftest test-predictions
+  (testing "prediction creation"
+    (jdbc/with-db-transaction [t-conn *db*]
+                              (let [prev-user (db/get-user-by-email
+                                                t-conn
+                                                {:email "pred.creation@example.com"})]
+                                (db/delete-user!
+                                  t-conn
+                                  {:id (:id prev-user)}))
+                              (db/create-user!
+                                t-conn
+                                {:first_name "Test"
+                                 :last_name  "Test"
+                                 :email      "pred.creation@example.com"
+                                 :password   (hashers/derive "test")})
+                              (let [user (db/get-user-by-email
+                                           t-conn
+                                           {:email "pred.creation@example.com"})]
+                                (is (= 1 (db/create-prediction!
+                                           t-conn
+                                           {:landmark    "Test"
+                                            :probability 100
+                                            :image       "https://test-prediciton-creation.com"
+                                            :user_id     (:id user)}))))))
+
+  (testing "prediction retrieval by user id"
+    (jdbc/with-db-transaction [t-conn *db*]
+                              (let [prev-user (db/get-user-by-email
+                                                t-conn
+                                                {:email "pred.retrieval@example.com"})]
+                                (db/delete-user!
+                                  t-conn
+                                  {:id (:id prev-user)}))
+                              (db/create-user!
+                                t-conn
+                                {:first_name "Test"
+                                 :last_name  "Test"
+                                 :email      "pred.retrieval@example.com"
+                                 :password   (hashers/derive "test")})
+                              (let [user (db/get-user-by-email
+                                           t-conn
+                                           {:email "pred.retrieval@example.com"})
+                                    user-id (:id user)]
+                                (db/create-prediction!
+                                  t-conn
+                                  {:landmark    "Test"
+                                   :probability 100
+                                   :image       "https://test-prediction-retrieval.com"
+                                   :user_id     user-id})
+                                (is (= false
+                                       (empty? (db/get-predictions-for-user
+                                                 t-conn
+                                                 {:user_id user-id}))))))))
