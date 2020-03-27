8ac3b1b3457d819d879a55f923a01ff8e13203c3
Milan <milan_susa@hotmail.com>
2020-03-27 14:13:11 +0100

    Load Workbox via CDN and add caching for static assets.

--- resources/public/service-worker.js
+++ resources/public/service-worker.js
@@ -1,3 +1,27 @@
-self.addEventListener('install', event => {});
+if( 'function' === typeof importScripts) {
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('fetch', event => {});
+    const {registerRoute} = workbox.routing;
+    const {StaleWhileRevalidate, CacheFirst} = workbox.strategies;
+    const {ExpirationPlugin} = workbox.expiration;
+
+    registerRoute(
+        /\.css$/,
+        new StaleWhileRevalidate({
+            cacheName: 'css-cache',
+        })
+    );
+
+    registerRoute(
+        /\.(?:png|jpg|jpeg|svg|gif)$/,
+        new CacheFirst({
+            cacheName: 'image-cache',
+            plugins: [
+                new ExpirationPlugin({
+                    maxEntries: 20,
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                })
+            ],
+        })
+    );
+}
